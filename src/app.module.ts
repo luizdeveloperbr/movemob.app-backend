1ed41508aa33eaf5190eb6252f5c209dcd3468bd
Luiz Eduardo <l.eduardos.lopes@gmail.com>
2023-04-04 04:31:19 +0000

    fix: directory struture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'
-import { PrismaService } from './database/prisma/prisma.service';
-import { EquipamentoController } from './entities/equipamento/equipamento.controller';
-import { EquipamentoService } from './entities/equipamento/equipamento.services';
-import { MovimentacaoController } from './entities/movimentacao/movimentacao.controller';
-import { MovimentacaoService } from './entities/movimentacao/movimentacao.service';
+//import { EquipamentoController } from './entities/equipamento/equipamento.controller';
+//import { EquipamentoService } from './entities/equipamento/equipamento.services';
+//import { MovimentacaoController } from './entities/movimentacao/movimentacao.controller';
+//import { MovimentacaoService } from './entities/movimentacao/movimentacao.service';
+import { EquipamentoModule } from './equipamento/equipamento.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [EquipamentoController, MovimentacaoController],
-  providers: [PrismaService, EquipamentoService, MovimentacaoService],
+  imports: [ConfigModule.forRoot(), EquipamentoModule],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule { }
